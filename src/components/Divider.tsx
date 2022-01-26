5cf86be706452ad22783186789bb4254a0b3300a
Yevhenii <yevhenii.bakhmat@nure.ua>
2022-01-26 16:15:46 +0200

    Made a specific component for divider

---
+++ src/components/Divider.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Divider = () => {
+  return (
+    <div className="bg-[#757391] text-2xl aspect-square w-[36px] grid place-items-center rounded-md">
+      /
+    </div>
+  );
+};
+
+export default Divider;
