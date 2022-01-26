1e5e9751931d8b4aacd0df410eb561645b8355af
Yevhenii <yevhenii.bakhmat@nure.ua>
2022-01-26 15:35:45 +0200

    Started working on HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+const HomePage = () => {
+  return (
+    <div className="w-screen h-screen grid place-items-center bg-gradient-to-br from-[#0D324D] to-[#7F5A83]">
+      <div className="bg-white flex flex-row gap-[8px]">
+        <input />
+        <span className="bg-[#757391] text-2xl aspect-square w-[36px] grid place-items-center">
+          /
+        </span>
+        <input />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
