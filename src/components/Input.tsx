406d2875a1109972956cfcffa7b33aa59e24a221
Yevhenii Bakhmat <yevhenii.bakhmat@nure.ua>
2022-01-26 19:29:11 +0200

    Create component for input fields

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,20 @@
-import React, { FC } from "react";
+import React, { ChangeEventHandler, FC } from "react";
 import styles from "../styles/Input.module.scss";
+
+type onInputChange = ChangeEventHandler<HTMLInputElement>;
+
 type Props = {
   label: string;
+  onChange?: onInputChange;
+  value?: string | number;
+  name: string;
 };
-const Input: FC<Props> = ({ label = "label" }) => {
-  return (
-    <div className={styles["group"]}>
-      <input type="text" required />
-      <span className={styles["bar"]}></span>
-      <label>{label}</label>
-    </div>
-  );
-};
+const Input: FC<Props> = ({ label = "label", onChange, value, name }) => (
+  <div className={styles["group"]}>
+    <input type="text" required onChange={onChange} value={value} name={name} />
+    <span className={styles["bar"]}></span>
+    <label>{label}</label>
+  </div>
+);
 
 export default Input;
