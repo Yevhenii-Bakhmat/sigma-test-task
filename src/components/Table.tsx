8bf564c1e7b50392f05195584d0baf58763349dc
Yevhenii Bakhmat <yevhenii.bakhmat@nure.ua>
2022-01-27 12:38:14 +0200

    Add onclick

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,11 @@
-import React, { FC } from "react";
-import { useSelector } from "react-redux";
+import React, { FC, MouseEvent } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Issue } from "../models/Issue";
-import { selectIssueCount, selectIssues } from "../store/repository";
+import {
+  getRepoInfoAsync,
+  selectIssueCount,
+  selectIssues,
+} from "../store/repository";
 import TableRow from "./TableRow";
 
 type Props = {
@@ -11,9 +15,13 @@ type Props = {
 const Table: FC<Props> = ({ className }) => {
   const issues = useSelector(selectIssues);
   const issueCount = useSelector(selectIssueCount);
+  const dispatch = useDispatch();
   const thStyle =
     " text-white bg-[#252937] text-left p-4 first:text-center first:border-l-[0] first:rounded-l-lg last:border-r-[0] last:rounded-r-lg border-x-[1px]";
-
+  const handleHeaderClick = (event: any) => {
+    const column = event.target.id;
+    // dispatch(getRepoInfoAsync(query));
+  };
   return (
     <table
       className={`w-11/12 ${className} gap-5 border-separate`}
@@ -22,10 +30,26 @@ const Table: FC<Props> = ({ className }) => {
     >
       <thead className="sticky top-0 ">
         <tr>
-          <th className={`${thStyle} w-[3%]`}></th>
+          <th
+            className={`${thStyle} w-[3%]`}
+            onClick={handleHeaderClick}
+            id="icon"
+          ></th>
           <th className={thStyle}>Title</th>
-          <th className={`${thStyle} w-[5%]`}>Label</th>
-          <th className={`${thStyle} w-[5%]`}>Assignee</th>
+          <th
+            className={`${thStyle} w-[5%]`}
+            onClick={handleHeaderClick}
+            id="labels"
+          >
+            Labels
+          </th>
+          <th
+            className={`${thStyle} w-[5%]`}
+            onClick={handleHeaderClick}
+            id="assignee"
+          >
+            Assignee
+          </th>
           <th className={`${thStyle} w-[2%]`}>
             <i className="far fa-comment text-white" />
           </th>
